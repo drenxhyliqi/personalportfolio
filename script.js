6afb1948d296e3be2ce5bb0b2847f2beec2c4e13
drenxhyliqi <113259185+drenxhyliqi@users.noreply.github.com>
2024-02-24 01:18:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -89,3 +89,11 @@ function scrollToTop() {
     behavior: "smooth", // Smooth scrolling behavior
   });
 }
+/*  COSTUM CURSOR */
+
+var cursor = document.querySelector(".cursor");
+var cursor2 = document.querySelector(".cursor2");
+document.addEventListener("mousemove", function (e) {
+  cursor.style.cssText = cursor2.style.cssText =
+    "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+});
